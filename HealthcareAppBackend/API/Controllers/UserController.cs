5440388c31b9cad3f3076fb943d1ab64dc491da5
YuliaGV <74123271+YuliaGV@users.noreply.github.com>
2023-12-22 18:21:40 -0500

    Initial commit

---
+++ HealthcareAppBackend/API/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _db;
+
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User> >> GetUsers()
+        {
+            var users = await _db.Users.ToListAsync();
+            return Ok(users);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser(Guid id)
+        {
+            var user = await _db.Users.FindAsync(id);
+            return Ok(user);
+        }
+
+
+    }
+}
